4d046e5521f4eb18a124fa97c0f61b2d5f1eed8f
Toby Twigger <tobytwigger1@gmail.com>
2022-07-26 13:04:17 +0100

    Override the models the prune command looks for

--- src/Support/Config.php
+++ src/Support/Config.php
@@ -4,8 +4,18 @@ namespace Maize\PrunableFields\Support;
 
 class Config
 {
+    protected static \Closure $callback;
+
+    public static function resolvePrunableModelsUsing(\Closure $callback)
+    {
+        static::$callback = $callback;
+    }
+
     public static function getPrunableModels(): array
     {
+        if(isset(static::$callback)) {
+            return call_user_func(static::$callback);
+        }
         return config('prunable-fields.models', []);
     }
 }
